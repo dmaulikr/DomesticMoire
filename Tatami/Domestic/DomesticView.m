159ec161e76bfcf4ca4bc40c014e7ad140c9cf43
Patrick Winchell <pat@tekgo.org>
2016-01-20 23:29:50 -0800

    - Properly include modified versions of the Domestic swift files. - Change some build settings. - Include XXIIVV logo as sprite that fades. - Add code to get the screensaver to include a sprite kit view correctly.

--- Tatami/Domestic/DomesticView.m
+++ Tatami/Domestic/DomesticView.m
@@ -3,6 +3,7 @@
 //  Copyright © 2016 Devine Lu Linvega. All rights reserved.
 
 #import "DomesticView.h"
+#import "Domestic-Swift.h"
 
 @implementation DomesticView
 
@@ -10,10 +11,27 @@
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
+        self.spriteView = [[ScreenSaverSKView alloc] initWithFrame:self.frame];
+        [self addSubview:self.spriteView];
+        self.spriteView.ignoresSiblingOrder = true;
+        self.spriteView.showsFPS = false;
+        self.spriteView.showsNodeCount = false;
+        self.spriteScene = [ScreenSaverScene new];
+        self.spriteScene.size = self.frame.size;
+        self.spriteScene.scaleMode = SKSceneScaleModeAspectFit;
+        self.spriteScene.backgroundColor = [SKColor blackColor];
+        [self.spriteView presentScene:self.spriteScene];
+        
         [self setAnimationTimeInterval:1/30.0];
     }
-	[[NSWorkspace sharedWorkspace] launchApplication: @"Domestic Moire"];
     return self;
+}
+
+-(void)setFrame:(NSRect)frame
+{
+    [super setFrame:frame];
+    self.spriteView.frame = self.frame;
+    self.spriteScene.size = self.frame.size;
 }
 
 - (void)startAnimation
@@ -33,6 +51,7 @@
 
 - (void)animateOneFrame
 {
+    [super animateOneFrame];
     return;
 }
 
---
+++ Tatami/Domestic/Tile.swift
@@ -0,0 +1,55 @@
+
+//  Created by Devine Lu Linvega on 2016-01-02.
+//  Copyright © 2016 Devine Lu Linvega. All rights reserved.
+
+import Foundation
+import SpriteKit
+
+class Tile: SKNode
+{
+	var sprite:SKShapeNode!
+	var time:CGFloat = 0
+	var index:Int = 0
+	var ratio:CGFloat = 0
+	
+    init( index:Int, size: NSSize)
+	{
+		super.init()
+		self.position = CGPoint(x: size.width/2, y: size.height/2)
+		self.index = index
+		
+		sprite = SKShapeNode(circleOfRadius: (size.height/100) * CGFloat(rings - index))
+		sprite.fillColor = index % 2 == 0 ? NSColor.whiteColor() : NSColor.blackColor()
+		sprite.strokeColor = NSColor.clearColor()
+		addChild(sprite)
+		
+		ratio = CGFloat(index)/CGFloat(rings)
+	}
+	
+	func animate()
+	{
+		let slow = sin(time * 0.0001)
+		let medium = sin(time * 0.001)
+		let fast = sin(time * ratio * (medium/10))/ratio * 20 * ratio * slow
+		
+		let wobble = ((sin(time * 0.01) * CGFloat(rings) ) + CGFloat(rings)) * 0.1
+		
+		sprite.position = CGPoint(x: 0, y: (10 * ratio) + (10 * wobble * ratio))
+		
+		zRotation = time * 0.025 + ratio * (wobble/2) + (slow * ratio) * (slow * wobble/medium)
+		sprite.xScale = 1 + (wobble * ratio * 0.1)
+		sprite.yScale = 1 + (wobble * ratio * 0.2 * ratio * wobble * 0.25)
+		
+		sprite.xScale += fast * 0.01 * ratio
+		sprite.yScale += fast * 0.01 * ratio
+		
+		sprite.zRotation = ( wobble * ratio * medium * wobble ) / ratio
+		
+		time += 1
+	}
+
+	required init?(coder aDecoder: NSCoder)
+	{
+	    fatalError("init(coder:) has not been implemented")
+	}
+}
